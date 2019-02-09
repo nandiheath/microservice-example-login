a4549fcd24bf024dea41cb4043ff95a0afd8ac5d
Nandiheath <nandi0315@gmail.com>
2019-02-09 15:22:51 +0800

    update api docs

--- src/route.ts
+++ src/route.ts
@@ -10,55 +10,57 @@ const authController = require('./controllers/auth_controller');
 
 export const route = (server) => {
 
+
   /**
    *
-   * @api {post} /auth/facebook Facebook Login
-   * @apiName FacebookLogin
+   * @api {post} /register Register
+   * @apiName Register
    * @apiGroup auth
    * @apiVersion  1.0.0
    * @apiHeader (AuthHeader) {String} Content-Type application/json
    * @apiParamExample {json} Request Example:
-                     {
-                       access_token: ''
+                    {
+                       username: 'user',
+                       password: 'pass'
                     }
   * @apiSuccessExample {type} Success-Response:
   * {
   *     success: true,
   *     data: {
   *        user: {
-  *            id: String,
   *            username: String
   *        },
-  *        token: String
+  *        access_token: String
+  *      }
+  * }
+  */
+ server.post('/register', asyncMiddleware(authController.register));
+
+  /**
+   *
+   * @api {post} /login Login
+   * @apiName Login
+   * @apiGroup auth
+   * @apiVersion  1.0.0
+   * @apiHeader (AuthHeader) {String} Content-Type application/json
+   * @apiParamExample {json} Request Example:
+                    {
+                       username: 'user',
+                       password: 'pass'
+                    }
+  * @apiSuccessExample {type} Success-Response:
+  * {
+  *     success: true,
+  *     data: {
+  *        user: {
+  *            username: String
+  *        },
+  *        access_token: String
   *      }
   * }
   */
   server.post('/login', asyncMiddleware(authController.login));
 
 
-  /**
-   *
-   * @api {post} /auth/facebook Facebook Login
-   * @apiName FacebookLogin
-   * @apiGroup auth
-   * @apiVersion  1.0.0
-   * @apiHeader (AuthHeader) {String} Content-Type application/json
-   * @apiParamExample {json} Request Example:
-                     {
-                       access_token: ''
-                    }
-  * @apiSuccessExample {type} Success-Response:
-  * {
-  *     success: true,
-  *     data: {
-  *        user: {
-  *            id: String,
-  *            username: String
-  *        },
-  *        token: String
-  *      }
-  * }
-  */
-  server.post('/register', asyncMiddleware(authController.register));
   // server.get('/me', passport.authenticate('jwt'), asyncMiddleware(authController.me));
 };
