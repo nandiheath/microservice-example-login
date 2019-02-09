34b794b24234fdea481a8bd50303fa947f94cd36
Nandiheath <nandi0315@gmail.com>
2019-02-09 14:50:36 +0800

    update env and config files

--- server.config.js
+++ server.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'oibs-api',
-    script: './src/server.js',
+    script: './built/server.js',
     watch: false,
     max_restarts: 5,
     restart_delay: 1000
--- src/common/env.ts
+++ src/common/env.ts
@@ -1,4 +1,5 @@
 export const ENV = process.env.ENV || 'dev';
 export const JWT_SECRET = process.env.JWT_SECRET || 'random_secret';
 export const LOGGER_LEVEL = process.env.LOGGER_LEVEL || 'debug';
-
+export const SERVER_HOST: string = process.env.SERVER_HOST || '127.0.0.1';
+export const SERVER_PORT: Number = parseInt(process.env.SERVER_PORT, 10) || 6010;
