04d9058980cb14b5edaacc5bee548f1d2d937a2b
Nandiheath <nandi0315@gmail.com>
2019-02-09 14:54:47 +0800

    use console as default for logger

--- server.config.js
+++ server.config.js
@@ -4,6 +4,9 @@ module.exports = {
     script: './built/server.js',
     watch: false,
     max_restarts: 5,
-    restart_delay: 1000
+    restart_delay: 1000,
+    env: {
+      "NODE_ENV": "production"
+    },
   }],
 };
